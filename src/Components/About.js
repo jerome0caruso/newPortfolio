b9a5d04839114021d75bedbbf4d5bcaf833a7b80
Jerome Caruso <jerome0caruso@gmail.com>
2020-11-26 20:15:10 -0600

    Added new project Musify to portfolio

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
          var zip = this.props.data.address.zip;
          var phone = this.props.data.phone;
          var email = this.props.data.email;
-         var resumeDownload = this.props.data.resumedownload;
+         var resumeDownload = 'http://www.myjavascriptprojects.com/resume/Jerome%20Caruso%20Resume.pdf'
       }
 
       return (
@@ -40,7 +40,7 @@ class About extends Component {
                      </div>
                      <div className="columns download">
                         <p>
-                           <a href={/*resumeDownload*/'#'} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                           <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
                         </p>
                      </div>
                   </div>
